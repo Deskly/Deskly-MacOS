243cccebac17e2fe491e367ed7d50f270758e952
Kaioru <keifuurawrs@gmail.com>
2016-12-09 20:34:53 +0800

    Initial Commit

---
+++ Deskly-MacOS/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Deskly-MacOS
+//
+//  Created by Keith Toh on 9/12/16.
+//  Copyright © 2016 Kaioru. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
